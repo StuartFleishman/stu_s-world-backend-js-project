3bb62c8621e4c44698326dceb20a614b5ba6d2ea
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-04-14 17:27:55 -0400

    made users/comments model and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+john = User.create(name: "stuart")
+firstComment = Comment.create(text: "hey now", user_id: 1)
