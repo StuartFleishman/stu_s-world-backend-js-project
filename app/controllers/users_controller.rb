02c2925875c7078fa9a225da039c1138de86416b
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-04-14 19:17:26 -0400

    added create action and user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+  def index 
+    users = User.all 
+    render json: users.to_json(except: [:created_at, :updated_at], include: {comments: {only: [:text] }} )
+  end 
+
+  def create 
+    user = User.new(user_params)
+  end 
+
+  private 
+
+  def user_params 
+    params.require(:user).permit(:name)
+  end
+
+
 end
